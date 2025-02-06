ec681e287e494244c5a98dba60418a06adae293b
BlackSonoJ <107205111+BlackSonoJ@users.noreply.github.com>
2025-02-06 21:56:11 +0200

    finally understood why photos were saved wrong way and fixed it, next time will add images to db

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Images
+from .models import Images, Events, ImagesMainPage
 
 
 class ImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Images
         fields = ("id", "image", "published")
+
+
+class ImagesMainPageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImagesMainPage
+        fields = ("id", "image", "published")
+
+
+class EventsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Events
+        fields = ("id", "name", "description", "published")
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,31 @@
 from rest_framework import viewsets, permissions
 
-from .models import Images
-from .serializers import ImagesSerializer
+from .models import Images, ImagesMainPage, Events
+from .serializers import ImagesSerializer, ImagesMainPageSerializer, EventsSerializer
 
 
-class ImageStorageViewSet(viewsets.ModelViewSet):
-    """Представление для профилей пользователей"""
+class ImageViewSet(viewsets.ModelViewSet):
 
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ImageMainPageViewSet(viewsets.ModelViewSet):
+    queryset = ImagesMainPage.objects.all()
+    serializer_class = ImagesMainPageSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class EventsViewSet(viewsets.ModelViewSet):
+    queryset = Events.objects.all()
+    serializer_class = EventsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
